688c516f49567361aa5cd2df6d4fddf0436be444
lsy26499 <lsy26499@gmail.com>
2020-07-20 21:30:59 +0900

    :sparkles: Add StationDetail page structure
    - 역 상세 페이지의 기본 구조를 업데이트했습니다.

---
+++ StationDetailSlider.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import ViewPager from '@react-native-community/viewpager';
+
+import StationDetailCard from './StationDetailCard';
+
+const StationDetailSlider = ({ detailInfo }) => {
+	return (
+		<View style={styles.cardsContainer}>
+			<ViewPager style={styles.viewPager}>
+				{detailInfo.map((info, idx) => (
+					<StationDetailCard key={idx} info={info} />
+				))}
+			</ViewPager>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	viewPager: {
+		marginHorizontal: 20,
+		flex: 0.8,
+	},
+
+	cardsContainer: {
+		flex: 1,
+		alignSelf: 'stretch',
+	},
+});
+
+export default StationDetailSlider;
