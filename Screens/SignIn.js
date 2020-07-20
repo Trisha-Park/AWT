052e73703dfd371ae50dd0f6bacf741629b3a638
Trisha Park <61039975+Trisha-Park@users.noreply.github.com>
2020-07-20 12:27:28 +0900

    :art: Fix style
    - 스타일을 변경하였습니다.

--- Screens/SignIn.js
+++ Screens/SignIn.js
@@ -4,21 +4,30 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 const SignIn = () => {
     return (
         <View style={styles.container}>
-            <Text>로그인페이지입니다</Text>
-            <Button title='구글' />
-            <Button title='카카오' />
-            <Button title='네이버' />
-            <Button title='비로그인으로 둘러보기' />
+            <Button color='red' style={styles.google} title='구글' />
+            <Button color='gold' style={styles.kakao} title='카카오' />
+            <Button color='skyblue' style={styles.naver} title='네이버' />
+            <Button
+                color='magenta'
+                style={styles.noneLogin}
+                title='비로그인으로 둘러보기'
+            />
         </View>
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+    google: {
+        
+    },
+    kakao: {
+
+    },
+    naver: {
+
+    },
+    noneLogin: {
+
     },
 });
 
