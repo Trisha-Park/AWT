1bfdc514fed9e8881b7a74468a8efff8d9a79ded
lsy26499 <lsy26499@gmail.com>
2020-07-20 14:18:23 +0900

    :art: Update Main page structure
    - 메인 페이지 기본 틀을 업데이트했습니다.

--- Screens/Main.js
+++ Screens/Main.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import { StyleSheet, View, Text } from 'react-native';
 
 // react navigation 적용시 서치바를 스택 네비게이션의 상단 바로 어떻게 빼야 할 지 생각해보기
 const Main = () => {
 	return (
 		<>
-			<View style={styles.searchBar}></View>
+			<View style={styles.searchBar}>
+				<Text style={styles.logoText}>A Week Trip</Text>
+				<FontAwesome name='search' size={24} color='black' />
+			</View>
 			<View style={styles.plans}></View>
 			<View style={styles.regions}></View>
 			<View style={styles.courses}></View>
@@ -16,7 +20,16 @@ const Main = () => {
 const styles = StyleSheet.create({
 	searchBar: {
 		flex: 1,
-		backgroundColor: 'blue',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		backgroundColor: '#f5f6fa',
+		alignSelf: 'stretch',
+		paddingHorizontal: 20,
+	},
+	logoText: {
+		fontFamily: '',
+		fontSize: 20,
 	},
 	plans: {
 		flex: 4,
