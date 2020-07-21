dd797556d1646fc497d32589f6daedef1e2afb7d
Trisha Park <61039975+Trisha-Park@users.noreply.github.com>
2020-07-21 15:26:01 +0900

    :zap: Fix Community Main in Article
    - 커뮤니티 메인 탭의 Article 컴포를 추가하였습니다.

--- Screens/Community.js
+++ Screens/Community.js
@@ -1,31 +1,65 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { List } from 'native-base';
+import Articles from '../Articles';
 
-Community = () => {
+const dummy = [
+    {
+        title: 'H ㅏ... 살려줘라',
+        author: 'Trisha',
+        date: '2020.07.21',
+        data: '낵아 웨 후론투웬드를 간닥오 핵아직오',
+    },
+    {
+        title: '번개 ㄱ?',
+        author: '번개러버',
+        date: '2020.07.21',
+        data: '서울역 노숙 번개 구함',
+    },
+    {
+        title: '젲쭈또에써 여낀안또ㅒ요',
+        author: '까찌마쪠빨',
+        date: '2020.07.21',
+        data: '않이 진짜 여긴 안또ㅒ',
+    },
+];
+
+const Community = () => {
     return (
-        <View>
+        <View style={styles.article}>
             <TouchableOpacity>
-            <Text>누르면 아티클 컴포 들어갈 자리</Text>
-            <Text>아티클 컴포 들어가요</Text>
-            <Text>아티클 컴포 들어가요</Text>
-            <TouchableOpacity style={styles.button}>
-                <Text>🖋글쓰기</Text>
+                <List>
+                    {dummy.map((data, idx) => (
+                        <Articles key={idx} article={data}></Articles>
+                    ))}
+                </List>
             </TouchableOpacity>
-            </TouchableOpacity>
+            <View style={styles.fix}>
+                <TouchableOpacity style={styles.button}>
+                    <Text>🖋글쓰기</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    fix: {
+        position: 'absolute',
+        top: 650,
+        left: 57,
+    },
     button: {
         width: 300,
         backgroundColor: 'pink',
         alignItems: 'center',
         justifyContent: 'center',
-        alignContent : "center",
+        alignContent: 'center',
         padding: 8,
         borderRadius: 5,
-        marginTop : 600
+    },
+    article: {
+        marginTop: 100,
     },
 });
 
