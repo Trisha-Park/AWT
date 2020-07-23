a7e2320d8904fbe3c03ae53a73e6ab7d048e6365
Trisha Park <61039975+Trisha-Park@users.noreply.github.com>
2020-07-23 11:37:28 +0900

    :truck: Move Main Component
    - 메인에 관련 컴포넌트를 한 폴더에 모아주었습니다.

---
+++ Component/Main/Courses.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+import ViewPager from '@react-native-community/viewpager';
+
+import CourseCard from './CourseCard';
+
+// 추천코스 테스트를 위한 더미데이터입니다.
+const dummy = [
+    {
+        region: '서울',
+        hashTag: ['#서울', '#미세먼지', '#도망가', '#배고파요', '#피곤해요'],
+    },
+    {
+        region: '영지',
+        hashTag: ['#영지', '#다키스트던전', '#갓겜', '#서커스', '#같이해요'],
+    },
+];
+
+// 여기에 슬라이더 적용
+const Courses = () => {
+    return (
+        <>
+            <Text style={styles.title}>추천 코스</Text>
+            <ViewPager style={styles.viewPager}>
+                {dummy.map((course, idx) => (
+                    <CourseCard hashTags={course.hashTag} key={idx} />
+                ))}
+            </ViewPager>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    title: {
+        marginBottom: 20,
+    },
+    viewPager: {
+        flex: 0.8,
+        alignSelf: 'stretch',
+        marginHorizontal: 10,
+    },
+});
+
+export default Courses;
---
+++ Component/Main/Regions.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+// 추천지역 그거입니다 더미데이터
+const dummy = ['서울', '단양', '대구', '부산', '전주'];
+
+const Regions = () => {
+    return (
+        <>
+            <Text>추천 지역</Text>
+            <View style={styles.container}>
+                {dummy.map((region, idx) => (
+                    <View key={idx} style={styles.card}>
+                        <Text>{region}</Text>
+                    </View>
+                ))}
+            </View>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+    },
+    card: {
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+    },
+});
+
+export default Regions;
