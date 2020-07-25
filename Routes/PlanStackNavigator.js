7c8f59c1f04dffdb4f8527761c2d0593e7787dcb
lsy26499 <lsy26499@gmail.com>
2020-07-25 18:42:35 +0900

    :art: Update Plan Stack Navigator
    - Plan 페이지의 메인 화면에 계획 존재 여부에 따라 다른 초기 페이지를 렌더링하도록 스택 네비게이터를 수정했습니다.

--- Routes/PlanStackNavigator.js
+++ Routes/PlanStackNavigator.js
@@ -7,9 +7,11 @@ import PlanInfoDetail from '../Screens/Plan/PlanInfoDetail';
 const PlanStack = createStackNavigator();
 
 const PlanStackNavigator = () => {
+    const isPlan = true; // 현재 계획이 있는 지를 나타내는 임시 변수 (추후 asyncStorage / axios로 가져와지는 데이터가 있는지로 판단 예정입니다)
+
     return (
         <PlanStack.Navigator
-            initialRouteName='Plan'
+            initialRouteName={isPlan ? 'PlanInfo' : 'Plan'}
             screenOptions={({ route }) => {
                 if (route.name === 'Plan') {
                     return {
