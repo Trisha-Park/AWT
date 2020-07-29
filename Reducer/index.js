6210c75f890293bb28b72fa2b85460f1554e3395
Trisha Park <61039975+Trisha-Park@users.noreply.github.com>
2020-07-29 11:19:05 +0900

    :sparkles: Add code to Reducer Index
    - 앱에 임포트 하기 위해 리듀서 파일에 인덱스를 추가하였습니다.

--- Reducer/index.js
+++ Reducer/index.js
@@ -1,1 +1,16 @@
 //? 리덕스 리팩토링을 위한 리듀서 파일입니다.
+const reducerSample = (state = {}, action) => {
+    switch (action.type) {
+        case 'sample': {
+            return {
+                ...state,
+                value: action.value,
+            };
+        }
+        default: {
+            return state;
+        }
+    }
+};
+
+export default reducerSample;
