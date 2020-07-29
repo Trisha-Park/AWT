c6d100ee5d5d690568fd8ab566d1e56287a08401
Trisha Park <61039975+Trisha-Park@users.noreply.github.com>
2020-07-29 14:27:39 +0900

    :zap: Remove Provider and Store from App
    - App에서 index로 프로바이더와 스토어를 옮겨주기 위해 앱에서 제거하였습니다.

--- App.js
+++ App.js
@@ -3,8 +3,6 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Provider } from 'react-redux';
-import store from './Store/index';
 
 import Main from './Routes/MainStackNavigator';
 import Station from './Screens/Station/Station';
@@ -22,69 +20,64 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
     const [isLogin, setIsLogin] = useState(false);
-
-    return (
-        <Provider store={store}>
-            (isLogin ? (
-            <NavigationContainer>
-                <StatusBar backgroundColor='black' />
-                <Tab.Navigator
-                    screenOptions={({ route }) => ({
-                        tabBarIcon: ({ color, size }) => {
-                            if (route.name === 'Main') {
-                                return (
-                                    <Fontisto
-                                        name='train-ticket'
-                                        size={24}
-                                        color='grey'
-                                    />
-                                );
-                            } else if (route.name === 'Station') {
-                                return (
-                                    <Ionicons
-                                        name='ios-train'
-                                        size={30}
-                                        color='gray'
-                                    />
-                                );
-                            } else if (route.name === 'Plan') {
-                                return (
-                                    <MaterialIcons
-                                        name='event-note'
-                                        size={30}
-                                        color='grey'
-                                    />
-                                );
-                            } else if (route.name === 'Community') {
-                                return (
-                                    <MaterialIcons
-                                        name='dashboard'
-                                        size={24}
-                                        color='lightgrey'
-                                    />
-                                );
-                            } else if (route.name === 'MyPage') {
-                                return (
-                                    <FontAwesome
-                                        name='user'
-                                        size={24}
-                                        color='navy'
-                                    />
-                                );
-                            }
-                        },
-                    })}
-                >
-                    <Tab.Screen name='Main' component={Main} />
-                    <Tab.Screen name='Station' component={Station} />
-                    <Tab.Screen name='Plan' component={Plan} />
-                    <Tab.Screen name='Community' component={Community} />
-                    <Tab.Screen name='MyPage' component={MyPage} />
-                </Tab.Navigator>
-            </NavigationContainer>
-            ) : (
-            <SignIn isLogin={isLogin} setIsLogin={setIsLogin} />
-            ))
-        </Provider>
+    return isLogin ? (
+        <NavigationContainer>
+            <StatusBar backgroundColor='black' />
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ color, size }) => {
+                        if (route.name === 'Main') {
+                            return (
+                                <Fontisto
+                                    name='train-ticket'
+                                    size={24}
+                                    color='grey'
+                                />
+                            );
+                        } else if (route.name === 'Station') {
+                            return (
+                                <Ionicons
+                                    name='ios-train'
+                                    size={30}
+                                    color='gray'
+                                />
+                            );
+                        } else if (route.name === 'Plan') {
+                            return (
+                                <MaterialIcons
+                                    name='event-note'
+                                    size={30}
+                                    color='grey'
+                                />
+                            );
+                        } else if (route.name === 'Community') {
+                            return (
+                                <MaterialIcons
+                                    name='dashboard'
+                                    size={24}
+                                    color='lightgrey'
+                                />
+                            );
+                        } else if (route.name === 'MyPage') {
+                            return (
+                                <FontAwesome
+                                    name='user'
+                                    size={24}
+                                    color='navy'
+                                />
+                            );
+                        }
+                    },
+                })}
+            >
+                <Tab.Screen name='Main' component={Main} />
+                <Tab.Screen name='Station' component={Station} />
+                <Tab.Screen name='Plan' component={Plan} />
+                <Tab.Screen name='Community' component={Community} />
+                <Tab.Screen name='MyPage' component={MyPage} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    ) : (
+        <SignIn isLogin={isLogin} setIsLogin={setIsLogin} />
     );
 }
