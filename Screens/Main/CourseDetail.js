1c6289ccdfd4d33759bff471fcf36c9a7b9429ad
lsy26499 <lsy26499@gmail.com>
2020-08-01 19:22:29 +0900

    :sparkles: Update main page best course axios
    - 메인 페이지의 추천코스와 상세 페이지의 axios 작업을 수행했습니다.

--- Screens/Main/CourseDetail.js
+++ Screens/Main/CourseDetail.js
@@ -1,21 +1,45 @@
-import React, { useState } from 'react';
-import { StyleSheet, ScrollView, SafeAreaView, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, ScrollView, SafeAreaView, View, Text } from 'react-native';
 import Constants from 'expo-constants';
+import axios from 'axios';
 
 import CourseDetailCard from '../../Component/Main/CourseDetailCard';
 import { courseDetailDummy } from '../../FakeData/mainData';
 
 const CourseDetail = ({ route }) => {
-    console.log(route.params);
-    const [couresDetail, setCourseDetail] = useState([...courseDetailDummy]);
+    const {
+        params: { num },
+    } = route;
+    const [couresDetail, setCourseDetail] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-    return (
+    const getCourses = async () => {
+        try {
+            setIsLoading(true);
+            const { data } = await axios.get(
+                `http://192.168.0.40:5050/bestplan/${num}`
+            );
+            setCourseDetail([...data[0].plan]);
+            setIsLoading(false);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getCourses();
+    }, []);
+
+    return isLoading ? (
+        <View>
+            <Text>로딩중..</Text>
+        </View>
+    ) : (
         <SafeAreaView styles={styles.container}>
             <ScrollView contentContainerStyle={styles.scrollView}>
                 {couresDetail.map((courseInfo, idx) => (
                     <CourseDetailCard courseInfo={courseInfo} key={idx} />
                 ))}
-                <Button title='내 계획으로' />
             </ScrollView>
         </SafeAreaView>
     );
@@ -27,7 +51,7 @@ const styles = StyleSheet.create({
     },
     scrollView: {
         alignItems: 'center',
-        paddingTop: Constants.statusBarHeight,
+        paddingVertical: Constants.statusBarHeight,
     },
 });
 
