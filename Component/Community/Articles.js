5587949f9a516209bd2ddba2a64dbc46175b089b
Trisha Park <61039975+Trisha-Park@users.noreply.github.com>
2020-08-01 17:39:30 +0900

    :zap: Update Data Value
    - DB의 컬럼과 데이터 형식을 맞춰 변경하였습니다,

--- Component/Community/Articles.js
+++ Component/Community/Articles.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { Left, ListItem, Right } from 'native-base';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Left, Right } from 'native-base';
 
 const Articles = ({ article }) => {
-    const { author, date, title } = article;
+    const { createdAt, title, name} = article;
 
     // TODO: 리액트 네비게이터를 이용해서 ArticleDetail 페이지로 넘어가는 것을 구현해 주세요.
     // TODO: ArticleDetail 페이지로 받아온 개별 article의 내용들을 넘겨주세요
@@ -14,8 +14,8 @@ const Articles = ({ article }) => {
                 <Text>{title}</Text>
             </Left>
             <Right>
-                <Text>{date}</Text>
-                <Text>{author}</Text>
+                <Text>{createdAt}</Text>
+                <Text>{name}</Text>
             </Right>
         </View>
     );
