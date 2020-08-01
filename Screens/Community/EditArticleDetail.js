11477aed717643b98c72e7e047625eec1d782f99
Trisha Park <61039975+Trisha-Park@users.noreply.github.com>
2020-08-01 17:46:52 +0900

    :sparkles: Add EditArticleDetail Screen & Add axios
    - 아티클 편집 페이지를 생성하고 악시오스 PUT요청을 추가하였습니다.

---
+++ Screens/Community/EditArticleDetail.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+
+import axios from 'axios';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+const EditArticleDetail = ({ route }) => {
+    const [articleDetailForEdit, setArticleDetailForEdit] = useState({
+        ...route.params.articleDetail,
+    });
+    const [titleEdit, setTitleEdit] = useState(articleDetailForEdit.title);
+    const [contentEdit, setContentEdit] = useState(
+        articleDetailForEdit.article
+    );
+
+    const editArticle = async () => {
+        try {
+            const { data } = await axios.put(
+                `http://192.168.0.5:5050/community/${articleDetailForEdit._id}`,
+                {
+                    userId: 1,
+                    name: 'trisha',
+                    title: titleEdit,
+                    article: contentEdit,
+                }
+            );
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <>
+            <View>
+                <View style={styles.title}>
+                    <TextInput
+                        value={titleEdit}
+                        onChangeText={(text) => {
+                            setTitleEdit(text);
+                        }}
+                    ></TextInput>
+                </View>
+                <View>
+                    <View style={styles.text}>
+                        <TextInput
+                            value={contentEdit}
+                            onChangeText={(text) => {
+                                setContentEdit(text);
+                            }}
+                        ></TextInput>
+                    </View>
+                </View>
+                <TouchableOpacity
+                    onPress={() => {
+                        editArticle();
+                    }}
+                >
+                    <Text>저장하기</Text>
+                </TouchableOpacity>
+            </View>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    title: {
+        height: 40,
+        width: 400,
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        marginTop: 50,
+        padding: 10,
+    },
+    text: {
+        height: 300,
+        width: 400,
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        padding: 10,
+        marginTop: 10,
+    },
+    button: {
+        backgroundColor: 'pink',
+        width: 400,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 5,
+        marginTop: 3,
+    },
+});
+
+export default EditArticleDetail;
