7c3366daada3657082cc8086ad639111248a5ca9
lsy26499 <lsy26499@gmail.com>
2020-08-04 11:51:58 +0900

    :sparkles: Add FaceBook Auth Reducer
    - 페이스북 로그인 관련해서 리덕스 기본 셋팅을 추가했습니다.

--- Actions/authActions.js
+++ Actions/authActions.js
@@ -1,15 +1,35 @@
-export const GOOGLE_AUTH_REQUEST = 'GOOGLE_AUTH_REQUEST';
+export const GOOGLE_AUTH_START = 'GOOGLE_AUTH_REQUEST';
 export const GOOGLE_AUTH_SUCCESS = 'GOOGLE_AUTH_SUCCESS';
 export const GOOGLE_AUTH_FAILURE = 'GOOGLE_AUTH_FAILURE';
 
-export const FB_AUTH_REQUEST = 'FB_AUTH_REQUEST';
+export const FB_AUTH_START = 'FB_AUTH_REQUEST';
 export const FB_AUTH_SUCCESS = 'FB_AUTH_SUCCESS';
 export const FB_AUTH_FAILURE = 'FB_AUTH_FAILURE';
 
-export const KAKAO_AUTH_REQUEST = 'KAKAO_AUTH_REQUEST';
-export const KAKAO_AUTH_SUCCESS = 'KAKAO_AUTH_SUCCESS';
-export const KAKAO_AUTH_FAILURE = 'KAKAO_AUTH_FAILURE';
-
-export const SIGN_OUT_REQUEST = 'LOG_OUT_REQUEST';
+export const SIGN_OUT_START = 'LOG_OUT_REQUEST';
 export const SIGN_OUT_SUCCESS = 'LOG_OUT_SUCCESS';
 export const SIGN_OUT_FAILURE = 'LOG_OUT_FAILURE';
+
+export const FBAuthStart = (isLoggingIn) => ({
+    type: FB_AUTH_START,
+    isLoggingIn,
+});
+
+export const FBAuthSuccess = (
+    isLoggingIn,
+    userInfo,
+    authToken,
+    resourceToken
+) => ({
+    type: FB_AUTH_SUCCESS,
+    isLoggingIn,
+    userInfo,
+    authToken,
+    resourceToken,
+});
+
+export const FBAuthFailure = (isLoggingIn, fbAuthError) => ({
+    type: FB_AUTH_FAILURE,
+    isLoggingIn,
+    fbAuthError,
+});
--- Reducer/index.js
+++ Reducer/index.js
@@ -1,6 +1,8 @@
 import { planReducer } from './planReducer';
+import { authReducer } from './authReducer';
 import { combineReducers } from 'redux';
 
 export default combineReducers({
     planReducer,
+    authReducer,
 });
