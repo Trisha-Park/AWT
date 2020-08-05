78563fce3395cb2f6d733294f3067037cbf5b6b3
lsy26499 <lsy26499@gmail.com>
2020-08-06 02:20:55 +0900

    :construction: Update google login using deep link
    - 구글 로그인 부분을 딥링크를 이용해 연결했습니다.

--- Component/Auth/GoogleButton.js
+++ Component/Auth/GoogleButton.js
@@ -1,34 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as Google from 'expo-google-app-auth';
 import { Button } from 'react-native';
+import axios from 'axios';
+import * as Linking from 'expo-linking';
 
 const GoogleButton = () => {
+    const [toggleClick, setToggleClick] = useState(false);
+    const [src, setSrc] = useState(null);
     const ANDROID_CLIENT_ID =
         '499458411825-vqctkbs5ehf31ge8gpbnt28fq6nhpo68.apps.googleusercontent.com';
+    const IOS_CLIENT_ID =
+        'http://499458411825-5dhsuovaviqugpbdn71fkfob4h3rekn1.apps.googleusercontent.com/';
 
     const googleSignIn = async () => {
         try {
-            const data = await Google.logInAsync({
-                androidClientId: ANDROID_CLIENT_ID,
-            });
+            //* expo google package 사용
+            // // 여기에 ios 클라 아이디를 추가 & app.json 처리가 있으면 해줘야 합니다
+            // const data = await Google.logInAsync({
+            //     androidClientId: ANDROID_CLIENT_ID,
+            //     iosClientId: IOS_CLIENT_ID
+            // });
 
-            if (data.type === 'success') {
-                console.log(data);
-                // TODO: 써버한테 데이터 쏴주고나서 JWT 토큰 받으면.. 그때 유저정보는 asyncstorage에 / 토큰은.. 어따주지 헬데답변 대기중입니다
-            }
+            // if (data.type === 'success') {
+            //     console.log(data);
+            //     // 서버한테 axios 요청
+            // }
+
+            //* 웹뷰 사용
+            setToggleClick(true);
+
+            Linking.openURL('http://13.125.93.29:5050/user/google');
+            const response = await axios.get(
+                'http://13.125.93.29:5050/user/google/callback'
+            );
+            console.log(response);
+            // setSrc(response.data);
         } catch (err) {
             console.log(err);
         }
     };
 
     return (
-        <Button
-            color='pink'
-            title='구글'
-            onPress={() => {
-                googleSignIn();
-            }}
-        />
+        <>
+            <Button
+                color='pink'
+                title='구글'
+                onPress={() => {
+                    googleSignIn();
+                }}
+            />
+        </>
     );
 };
 
