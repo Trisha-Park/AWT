2a8c39fc0b430c977f0309cba596ef1351e8d5ed
lsy26499 <lsy26499@gmail.com>
2020-08-07 02:12:33 +0900

    :art: Update google signin component
    - 구글 로그인 부분의 비동기 요청을 수정했습니다.

--- Component/Auth/GoogleButton.js
+++ Component/Auth/GoogleButton.js
@@ -1,33 +1,41 @@
-import React, { useState } from 'react';
+import React from 'react';
 import * as Google from 'expo-google-app-auth';
 import { Button } from 'react-native';
-import * as Linking from 'expo-linking';
 import axios from 'axios';
+import { connect } from 'react-redux';
+import {
+    googleAuthStart,
+    googleAuthSuccess,
+    googleAuthFailure,
+} from '../../Actions/authActions';
 // 마무리 되면 axios 리팩토링!
 
-const GoogleButton = () => {
-    const [toggleClick, setToggleClick] = useState(false);
-    const [src, setSrc] = useState(null);
+const GoogleButton = ({
+    googleAuthStart,
+    googleAuthSuccess,
+    googleAuthFailure,
+}) => {
     const ANDROID_CLIENT_ID =
         '499458411825-vqctkbs5ehf31ge8gpbnt28fq6nhpo68.apps.googleusercontent.com';
     const IOS_CLIENT_ID =
-        'http://499458411825-5dhsuovaviqugpbdn71fkfob4h3rekn1.apps.googleusercontent.com/';
+        '499458411825-5dhsuovaviqugpbdn71fkfob4h3rekn1.apps.googleusercontent.com';
 
     const googleSignIn = async () => {
         try {
             //* expo google package 사용
-            // 여기에 ios 클라 아이디를 추가 & app.json 처리가 있으면 해줘야 합니다
-            const data = await Google.logInAsync({
+            const userData = await Google.logInAsync({
                 androidClientId: ANDROID_CLIENT_ID,
                 iosClientId: IOS_CLIENT_ID,
             });
 
-            if (data.type === 'success') {
-                console.log(data);
-                const { data } = axios.post(
-                    'http://192.168.0.40:5050/user/google',
+            if (userData.type === 'success') {
+                console.log(userData.accessToken);
+                console.log(userData);
+                const { data } = await axios.post(
+                    'http://172.30.1.17:5050/user/google',
                     {
-                        google: data.accessToken,
+                        google: userData.accessToken,
+                        googleName: userData.user.name,
                     },
                     {
                         withCredentials: true,
@@ -42,7 +50,6 @@ const GoogleButton = () => {
             //     'http://3.34.197.112:5050/user/google/callback'
             // )
             // console.log(response);
-
         } catch (err) {
             console.log(err);
         }
@@ -61,4 +68,21 @@ const GoogleButton = () => {
     );
 };
 
-export default GoogleButton;
+const mapDispatchToProps = (dispatch) => {
+    return {
+        googleAuthStart: () => dispatch(googleAuthStart(isLoggingIn)),
+        googleAuthSuccess: () =>
+            dispatch(
+                googleAuthSuccess(
+                    isLoggingIn,
+                    userInfo,
+                    authToken,
+                    resourceToken
+                )
+            ),
+        googleAuthFailure: () =>
+            dispatch(googleAuthFailure(isLoggingIn, googleAuthError)),
+    };
+};
+
+export default connect(null, mapDispatchToProps)(GoogleButton);
