ee7914a98b422a93946053b6249bdd8ff55ed740
lsy26499 <lsy26499@gmail.com>
2020-08-06 17:24:32 +0900

    :art: Update google signin using expo package
    - 엑스포 내장 패키지를 사용한 구글 로그인 코드를 추가해씃ㅂ니다.

--- Component/Auth/GoogleButton.js
+++ Component/Auth/GoogleButton.js
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as Google from 'expo-google-app-auth';
 import { Button } from 'react-native';
-
+import * as Linking from 'expo-linking';
+import axios from 'axios';
 // 마무리 되면 axios 리팩토링!
 
 const GoogleButton = () => {
+    const [toggleClick, setToggleClick] = useState(false);
+    const [src, setSrc] = useState(null);
     const ANDROID_CLIENT_ID =
         '499458411825-vqctkbs5ehf31ge8gpbnt28fq6nhpo68.apps.googleusercontent.com';
+    const IOS_CLIENT_ID =
+        'http://499458411825-5dhsuovaviqugpbdn71fkfob4h3rekn1.apps.googleusercontent.com/';
 
     const googleSignIn = async () => {
         try {
+            //* expo google package 사용
+            // 여기에 ios 클라 아이디를 추가 & app.json 처리가 있으면 해줘야 합니다
             const data = await Google.logInAsync({
                 androidClientId: ANDROID_CLIENT_ID,
+                iosClientId: IOS_CLIENT_ID,
             });
 
             if (data.type === 'success') {
                 console.log(data);
-                // TODO: 써버한테 데이터 쏴주고나서 JWT 토큰 받으면.. 그때 유저정보는 asyncstorage에 / 토큰은.. 어따주지 헬데답변 대기중입니다
+                const { data } = axios.post(
+                    'http://192.168.0.40:5050/user/google',
+                    {
+                        google: data.accessToken,
+                    },
+                    {
+                        withCredentials: true,
+                    }
+                );
             }
+
+            //* 웹뷰 사용
+
+            // Linking.openURL('http://3.34.197.112:5050/user/google');
+            // const response = await axios.get(
+            //     'http://3.34.197.112:5050/user/google/callback'
+            // )
+            // console.log(response);
         } catch (err) {
             console.log(err);
         }
