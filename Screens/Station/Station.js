4077ca3d76b2e9e306dc1b00e1a5f6fae237a7f1
lsy26499 <lsy26499@gmail.com>
2020-08-07 22:06:58 +0900

    :pencil2: Fix typos
    - 내일로 -> 기차여행으로 수정했습니다.

--- Screens/Station/Station.js
+++ Screens/Station/Station.js
@@ -1,30 +1,28 @@
 import * as React from 'react';
-import { StyleSheet, View, Image, Text, Dimensions } from 'react-native';
+import { StyleSheet, View, Image, Dimensions } from 'react-native';
 import ImageZoom from 'react-native-image-pan-zoom';
 
-class Station extends React.Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <ImageZoom
-                    cropWidth={Dimensions.get('window').width}
-                    cropHeight={Dimensions.get('window').height}
-                    imageWidth={400}
-                    imageHeight={770}
-                >
-                    <Image
-                        source={require('../../img/map.png')}
-                        style={{
-                            width: '100%',
-                            height: '100%',
-                            resizeMode: 'contain',
-                        }}
-                    />
-                </ImageZoom>
-            </View>
-        );
-    }
-}
+const Station = () => {
+    return (
+        <View style={styles.container}>
+            <ImageZoom
+                cropWidth={Dimensions.get('window').width}
+                cropHeight={Dimensions.get('window').height}
+                imageWidth={400}
+                imageHeight={770}
+            >
+                <Image
+                    source={require('../../img/map.png')}
+                    style={{
+                        width: '100%',
+                        height: '100%',
+                        resizeMode: 'contain',
+                    }}
+                />
+            </ImageZoom>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
     container: {
