306c560a19b273005ec60753df480392f427f3a4
lsy26499 <lsy26499@gmail.com>
2020-08-07 18:26:40 +0900

    :fire: Remove Redux-logger
    -스토어에  리덕스 로거가 적용된 부분을 삭제했습니다.

--- Store/index.js
+++ Store/index.js
@@ -1,7 +1,6 @@
 import AsyncStorage from '@react-native-community/async-storage';
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
-import logger from 'redux-logger';
 
 import reducer from '../Reducer/index';
 
@@ -13,7 +12,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, reducer);
 
-const store = createStore(persistedReducer, applyMiddleware(logger));
+const store = createStore(persistedReducer);
 
 let persistor = persistStore(store);
 
