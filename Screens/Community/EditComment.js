91eafdf90cef75f90b50238f1dd2028c9f440738
Trisha Park <61039975+Trisha-Park@users.noreply.github.com>
2020-08-08 00:38:31 +0900

    :lipstick: Add EditComment Style
    - 코멘트 편집 페이지의 스타일을 추가하였습니다.

--- Screens/Community/EditComment.js
+++ Screens/Community/EditComment.js
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
-import { Text, View, TouchableOpacity, TextInput } from 'react-native';
-
+import {
+    Text,
+    View,
+    TouchableOpacity,
+    TextInput,
+    StyleSheet,
+} from 'react-native';
+import { connect } from 'react-redux';
 import axios from 'axios';
 
-const EditComment = ({ route }) => {
+const EditComment = ({ route, navigation, userInfo, resourceToken }) => {
     const [commentEdit, setCommentEdit] = useState(
         route.params.comments.comment
     );
@@ -15,35 +21,93 @@ const EditComment = ({ route }) => {
             const { data } = await axios.put(
                 `http://192.168.0.5:5050/comment/${route.params.communityId}/${route.params.commentId}`,
                 {
-                    userId: 1,
-                    name: 'trisha',
+                    userId: userInfo.userId,
+                    name: userInfo.name,
                     comment: commentEdit,
                     secret: false,
+                },                {
+                    headers: { authorization: resourceToken },
+                    withCredentials: true,
                 }
             );
         } catch (error) {
-            console.log(data);
+            console.log(error);
         }
     };
 
     return (
-        <View>
-            <View>
+        <View
+            style={{
+                flexDirection: 'column',
+                alignContent: 'center',
+                alignItems: 'center',
+                backgroundColor: '#ffffff',
+                flex: 1,
+            }}
+        >
+            <View style={styles.text}>
                 <TextInput
                     value={commentEdit}
+                    style={{
+                        width: 390,
+                        height: 280,
+                        justifyContent: 'flex-start',
+                        alignContent: 'flex-start',
+                        padding: 5,
+                        textAlignVertical: 'top',
+                        fontSize: 18,
+                    }}
                     onChangeText={(text) => setCommentEdit(text)}
-                    placeholder='hello'
                 ></TextInput>
             </View>
             <TouchableOpacity
+                style={styles.button}
                 onPress={() => {
                     editComment();
+                    navigation.navigate('ArticleDetail');
                 }}
             >
-                <Text>저장</Text>
+                <Text style={{ color: '#ffffff' }}>저장</Text>
             </TouchableOpacity>
         </View>
     );
 };
 
-export default EditComment;
+const styles = StyleSheet.create({
+    title: {
+        height: 50,
+        width: 400,
+        backgroundColor: '#F1F2F6',
+        justifyContent: 'center',
+        marginTop: 20,
+        padding: 10,
+    },
+    text: {
+        height: 300,
+        width: 400,
+        backgroundColor: '#F1F2F6',
+        alignContent: 'center',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+        marginTop: 5,
+    },
+    button: {
+        backgroundColor: '#0066FF',
+        width: 400,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 5,
+        marginTop: 3,
+    },
+});
+
+const mapStateToProps = (state) => {
+    return {
+        resourceToken: state.authReducer.resourceToken,
+        userInfo: state.authReducer.userInfo,
+    };
+};
+
+export default connect(mapStateToProps)(EditComment);
