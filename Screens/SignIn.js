a717c3ed5520359bf34936e9d4940297fa9e6613
lsy26499 <lsy26499@gmail.com>
2020-08-07 23:04:42 +0900

    :lipstick: Update signin Page styles
    - 로그인 페이지의 스타일을 업데이트했습니다.

--- Screens/SignIn.js
+++ Screens/SignIn.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Image, ImageBackground } from 'react-native';
+import { StyleSheet, View, ImageBackground, Text } from 'react-native';
 
 import GoogleButton from '../Component/Auth/GoogleButton';
 import FaceBookButton from '../Component/Auth/FaceBookButton';
@@ -10,6 +10,9 @@ const SignIn = () => {
             source={require('../assets/mainLoading.png')}
             style={styles.container}
         >
+            <View style={styles.titleContainer}>
+                <Text style={styles.title}>기차여행을 시작해보세요!</Text>
+            </View>
             <View style={styles.buttonContainer}>
                 <GoogleButton />
                 <FaceBookButton />
@@ -21,13 +24,23 @@ const SignIn = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'flex-end',
+        justifyContent: 'center',
         alignItems: 'center',
     },
     buttonContainer: {
         alignItems: 'center',
         justifyContent: 'center',
-        marginBottom: '30%',
+        flex: 1,
+    },
+    titleContainer: {
+        justifyContent: 'flex-end',
+        flex: 1,
+        marginTop: 30,
+    },
+    title: {
+        color: '#fff',
+        fontSize: 25,
+        fontWeight: 'bold',
     },
 });
 
