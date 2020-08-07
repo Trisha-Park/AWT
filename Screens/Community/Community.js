ed8581c7a39e9bd662deeb6ba2d03dfc2609c8cb
Trisha Park <61039975+Trisha-Park@users.noreply.github.com>
2020-08-07 17:28:48 +0900

    :lipstick: Add community style
    - 커뮤니티 메인 페이지에 스타일을 추가하였습니다. 스크롤링 가능.

--- Screens/Community/Community.js
+++ Screens/Community/Community.js
@@ -7,12 +7,14 @@ import {
     TextInput,
 } from 'react-native';
 
+import { EvilIcons } from '@expo/vector-icons';
 import Articles from '../../Component/Community/Articles';
-import Loading from "../../Screens/Loading"
+import Loading from '../../Screens/Loading';
 import axios from 'axios';
 import { connect } from 'react-redux';
 
 import { useIsFocused } from '@react-navigation/native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const Community = ({ navigation, resourceToken }) => {
     const [isArticleLoading, setIsArticleLoading] = useState(true);
@@ -23,7 +25,8 @@ const Community = ({ navigation, resourceToken }) => {
         try {
             setIsArticleLoading(true);
             const { data } = await axios.get(
-                'http://192.168.0.5:5050/community',{
+                'http://192.168.0.5:5050/community',
+                {
                     headers: { authorization: resourceToken },
                     withCredentials: true,
                 }
@@ -53,29 +56,27 @@ const Community = ({ navigation, resourceToken }) => {
     }, [isFocused]);
 
     return isArticleLoading ? (
-        <View style={styles.fix}>
             <Loading />
-            {/* <TouchableOpacity
-                style={styles.button}
-                onPress={() => {
-                    navigation.navigate('CreateArticle');
-                    navigation = { navigation };
-                }}
-            >
-                <Text>🖋글쓰기</Text>
-            </TouchableOpacity>
-            <Text>로딩중</Text> */}
-        </View>
     ) : (
-        <View>
-            <View>
+        <View
+            style={{
+                flexDirection: 'column',
+                alignItems: 'center',
+                backgroundColor: '#ffffff',
+            }}
+        >
+            <View style= {{flexDirection : "row", justifyContent : "space-between", alignSelf : "stretch", alignItems : "center", marginTop : 5}}>
+                <View style={styles.search}>
                 <TextInput
+                    style={styles.TextInput}
                     value={searchValue}
                     onChangeText={(text) => {
                         setSearchValue(text);
                     }}
                 ></TextInput>
-                <TouchableOpacity
+                </View>
+                <TouchableOpacity 
+                    style = {{width : 50, height :50, justifyContent : "center" , alignItems : "center", marginRight : 20, marginTop : 5 }}
                     onPress={() => {
                         navigation.navigate('CommunitySearch', {
                             searchValue: searchValue,
@@ -83,10 +84,10 @@ const Community = ({ navigation, resourceToken }) => {
                         });
                     }}
                 >
-                    <Text>검색</Text>
+                    <EvilIcons name='search' size={30} color='black' />
                 </TouchableOpacity>
             </View>
-            <View style={styles.article}>
+            <ScrollView style={styles.article}>
                 {articles.map((article, idx) => (
                     <TouchableOpacity
                         key={idx}
@@ -99,7 +100,7 @@ const Community = ({ navigation, resourceToken }) => {
                         <Articles article={article} />
                     </TouchableOpacity>
                 ))}
-            </View>
+            </ScrollView>
             <View style={styles.fix}>
                 <TouchableOpacity
                     style={styles.button}
@@ -108,7 +109,7 @@ const Community = ({ navigation, resourceToken }) => {
                         navigation = { navigation };
                     }}
                 >
-                    <Text>🖋글쓰기</Text>
+                    <Text style={{ color: '#ffffff' }}>글쓰기</Text>
                 </TouchableOpacity>
             </View>
         </View>
@@ -116,28 +117,51 @@ const Community = ({ navigation, resourceToken }) => {
 };
 
 const styles = StyleSheet.create({
+    search: {
+        flexDirection: 'row',
+        backgroundColor: '#F1F2F6',
+        borderRadius : 25,
+        justifyContent : "center",
+        alignItems : "center",
+        width : 330,
+        height : 50,
+        padding : 10,
+        margin : 10,
+        marginBottom : 5,
+        alignSelf : "stretch"
+    },
+    TextInput: {
+        width: 310,
+        height: 50,
+    },
+    searchText : {
+
+    },
     fix: {
         position: 'absolute',
-        top: 10,
-        left: 57,
+        bottom: 30,
+        alignContent: 'center',
+        alignItems: "flex-start",
+        justifyContent: 'center',
     },
     button: {
-        width: 300,
-        backgroundColor: 'pink',
+        width: 60,
+        backgroundColor: '#0066FF',
         alignItems: 'center',
         justifyContent: 'center',
         alignContent: 'center',
         padding: 8,
         borderRadius: 5,
+        bottom : 50
     },
     article: {
-        marginTop: 100,
+        marginTop: 10,
     },
 });
 
 const mapStateToProps = (state) => {
     return {
-        resourceToken : state.authReducer.resourceToken,
+        resourceToken: state.authReducer.resourceToken,
     };
 };
 
