0184c5990eee77f10aa2a8486931b53577a8b83a
lsy26499 <lsy26499@gmail.com>
2020-08-07 22:04:56 +0900

    :lipstick: Update signin pages styles
    - 로그인 페이지의 배경 이미지를 추가하고, 구글 로그인 버튼의 색상을 변경했습니다.

--- Screens/SignIn.js
+++ Screens/SignIn.js
@@ -1,44 +1,33 @@
 import React from 'react';
-import { StyleSheet, View, Image } from 'react-native';
+import { StyleSheet, View, Image, ImageBackground } from 'react-native';
 
 import GoogleButton from '../Component/Auth/GoogleButton';
 import FaceBookButton from '../Component/Auth/FaceBookButton';
 
 const SignIn = () => {
     return (
-        <View style={styles.container}>
-            <View
-                style={{
-                    flex: 4,
-                    alignItems: 'center',
-                    justifyContent: 'flex-end',
-                }}
-            >
-                <Image
-                    source={require('../assets/logo.png')}
-                    style={{ width: 150, height: 150 }}
-                />
-            </View>
-            <View
-                style={{
-                    flex: 4,
-                    alignItems: 'center',
-                    justifyContent: 'flex-start',
-                }}
-            >
+        <ImageBackground
+            source={require('../assets/mainLoading.png')}
+            style={styles.container}
+        >
+            <View style={styles.buttonContainer}>
                 <GoogleButton />
                 <FaceBookButton />
             </View>
-        </View>
+        </ImageBackground>
     );
 };
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'flex-end',
         alignItems: 'center',
-        backgroundColor: '#fff',
+    },
+    buttonContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: '30%',
     },
 });
 
