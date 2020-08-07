8ff4074b1a98f2ff7eb424d88c9f27f7f9bc2fc9
lsy26499 <lsy26499@gmail.com>
2020-08-07 14:20:19 +0900

    :lipstick: Update states of signin page
    - 로그인 페이지의 스타일을 수정했습니다.

--- Component/Auth/FaceBookButton.js
+++ Component/Auth/FaceBookButton.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Button } from 'react-native';
+import { StyleSheet, TouchableOpacity, Text } from 'react-native';
 import * as Facebook from 'expo-facebook';
 import axios from 'axios';
+
+import { AntDesign } from '@expo/vector-icons';
+
 import { connect } from 'react-redux';
 import {
     FBAuthStart,
@@ -19,65 +22,70 @@ const FaceBookButton = ({ FBAuthStart, FBAuthSuccess, FBAuthFailure }) => {
             const data = await Facebook.logInWithReadPermissionsAsync({
                 permissions: ['public_profile', 'email'],
             });
-            // console.log(data);
 
             if (data.type === 'success') {
-                // TODO: 페북에 토큰(or 데이터 전체)을 보내주기 (header 말고 body에 달라고 하십니다)
-                // 서버에서 응답을 받기
                 const userData = await axios.post(
                     'http://192.168.0.40:5050/user/facebook',
                     {
                         fbAccessToken: data.token,
                     },
                     {
-                        // headers: { authorization: data.token },
                         withCredentials: true,
                     }
                 );
-                // console.log('==========================');
-                // console.log(userData.data);
                 FBAuthSuccess(
                     false,
                     { name: userData.data.name, userId: userData.data.userId },
-                    data.token,
                     userData.data.token
                 );
-
-                // if('JWT 토큰을 제대로 받아왔을 경우') {
-                // asyncstorage에 토큰 및 유저 정보 저장 -> 닉네임 설정 창으로 이동 (닉네임 설정후 서버에 post 요청)
-                // -> (요청이 성공적이었다면) 리덕스의 isLogin 참으로 변경 -> 메인 화면으로 이동하기
-                // } else {
-                // 다시 로그인하라고 하기?
-                // }
             } else {
-                // 로그인 실패 -> 다시 로그인하라고 하기.. 로그인 페이지로 돌려보냄
-                FBAuthFailure(false, 'error');
+                FBAuthFailure(false);
                 alert('로그인에 실패했습니다.');
             }
         } catch (error) {
-            FBAuthFailure(false, error);
+            FBAuthFailure(false);
             alert(`Facebook Login Error: ${error}`);
         }
     };
 
     return (
-        <Button
-            color='skyblue'
+        <TouchableOpacity
             title='페이스북'
             onPress={() => {
                 FBSignIn();
             }}
-        />
+            style={styles.fbButton}
+        >
+            <AntDesign name='facebook-square' size={22} color='#fff' />
+            <Text style={styles.fbTitle}>페이스북으로 로그인하기</Text>
+        </TouchableOpacity>
     );
 };
+
+const styles = StyleSheet.create({
+    fbButton: {
+        width: 250,
+        paddingVertical: 10,
+        backgroundColor: '#405DAD',
+        marginVertical: 6,
+        borderRadius: 10,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    fbTitle: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 15,
+        marginLeft: 8,
+    },
+});
 
 const mapDispatchToProps = (dispatch) => {
     return {
         FBAuthStart: (isLoggingIn) => dispatch(FBAuthStart(isLoggingIn)),
-        FBAuthSuccess: (isLoggingIn, userInfo, authToken, resourceToken) =>
-            dispatch(
-                FBAuthSuccess(isLoggingIn, userInfo, authToken, resourceToken)
-            ),
+        FBAuthSuccess: (isLoggingIn, userInfo, resourceToken) =>
+            dispatch(FBAuthSuccess(isLoggingIn, userInfo, resourceToken)),
         FBAuthFailure: (isLoggingIn, fbAuthError) =>
             dispatch(FBAuthFailure(isLoggingIn, fbAuthError)),
     };
