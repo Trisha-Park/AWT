fb19f0aa8e97bd87ac669a963125aee9b18c1230
Trisha Park <61039975+Trisha-Park@users.noreply.github.com>
2020-08-09 02:14:37 +0900

    :lipstick: Add style in Article Component
    - 아티클 컴포넌트에 스타일을 추가하였습니다.

--- Component/Community/Articles.js
+++ Component/Community/Articles.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet} from 'react-native';
 
 const Articles = ({ article }) => {
-    const { updatedAt, title, name } = article;
-
+    const { updatedAt, title, name} = article;
+    //console.log(article);
     // TODO: 리액트 네비게이터를 이용해서 ArticleDetail 페이지로 넘어가는 것을 구현해 주세요.
     // TODO: ArticleDetail 페이지로 받아온 개별 article의 내용들을 넘겨주세요
 
     return (
         <View style={styles.article}>
-            <View style={{ height : 70, flexDirection: 'column', justifyContent : "space-between"}}>
+            <View
+                style={{
+                    height: 70,
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                }}
+            >
                 <Text style={styles.title}>{title}</Text>
                 <Text style={styles.name}>{name}</Text>
             </View>
-            <View >
+            <View>
                 <Text style={styles.date}>{updatedAt}</Text>
             </View>
         </View>
@@ -24,26 +30,27 @@ const styles = StyleSheet.create({
     article: {
         height: 90,
         width: 400,
-        flexDirection : "row",
-        alignItems : "flex-end",
-        justifyContent : "space-between",
-        backgroundColor : "#F1F2F6",
-        margin : 5,
-        padding : 10
-        
-
-
+        flexDirection: 'row',
+        alignItems: 'flex-end',
+        justifyContent: 'space-between',
+        backgroundColor: '#FFFFFF',
+        margin: 5,
+        padding: 10,
+        borderRadius: 20,
     },
-    title : {
-        fontSize : 24
+    title: {
+        fontSize: 24,
+        fontWeight : "bold"
     },
     name: {
-        fontStyle : "normal",
-        color : "#454545",
-        fontSize : 18
+        fontStyle: 'normal',
+        color: '#454545',
+        fontSize: 18,
+        color : "#787878"
     },
     date: {
-        fontSize : 18
+        fontSize: 18,
+        color : "#787878"
     },
 });
 
