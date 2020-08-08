d6343b7d6fa24a4c1f46747a261d2624439571f4
Trisha Park <61039975+Trisha-Park@users.noreply.github.com>
2020-08-09 02:15:13 +0900

    :lipstick: Add style in Comment
    - 댓글의 스타일을 추가하였습니다.

--- Component/Community/Comments.js
+++ Component/Community/Comments.js
@@ -1,20 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { ListItem } from 'native-base';
 import axios from 'axios';
 
 import { connect } from 'react-redux';
+import { AntDesign } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 
+const Comments = ({ comments, route, navigation, resourceToken, isCommentUser}) => {
 
-
-const Comments = ({ comments, route, navigation, resourceToken }) => {
-    console.log(comments._id);
     const commentDelete = async () => {
         try {
             const { data } = await axios.delete(
                 `http://192.168.0.5:5050/comment/${comments._id}`,
                 {
-                    headers: { authorization : resourceToken },
+                    headers: { authorization: resourceToken },
                     withCredentials: true,
                 }
             );
@@ -22,49 +21,66 @@ const Comments = ({ comments, route, navigation, resourceToken }) => {
             console.log(error);
         }
     };
-    //console.log(resourceToken);
-    
+
+  
+
     return (
-        <>
-            <TouchableOpacity style={styles.comment}>
-                <ListItem>
-                    <Text>{comments.name}</Text>
-                    <Text>{comments.comment}</Text>
-                </ListItem>
-                <TouchableOpacity
-                    onPress={() => {
-                        navigation.navigate('EditComment', {
-                            comments,
-                            communityId: route.params.id,
-                            commentId: comments._id,
-                        });
-                    }}
+        <View>
+            <View style={styles.comment}>
+                <View>
+                    <Text style={{ fontSize: 20, fontWeight: 'bold' }}>
+                        {comments.name}
+                    </Text>
+                </View>
+                <View>
+                    <Text style={{ fontSize: 16 }}>{comments.comment}</Text>
+                </View>
+{/* 
+                { isCommentUser ? ( */}
+                <View
+                    style={{ flexDirection: 'row', justifyContent: 'flex-end' }}
                 >
-                    <Text>편집</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={() => {
-                        commentDelete();
-                    }}
-                >
-                    <Text>삭제</Text>
-                </TouchableOpacity>
-            </TouchableOpacity>
-        </>
+                    <TouchableOpacity
+                        style={{ marginRight: 10 }}
+                        onPress={() => {
+                            navigation.navigate('EditComment', {
+                                comments,
+                                communityId: route.params.id,
+                                commentId: comments._id,
+                            });
+                        }}
+                    >
+                        <Feather name='edit' size={24} color='black' />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={{ marginRight: 5 }}
+                        onPress={() => {
+                            commentDelete();
+                        }}
+                    >
+                        <AntDesign name='delete' size={24} color='black' />
+                    </TouchableOpacity>
+                </View>
+                {/* //  ) : (<></>) }  */}
+            </View>
+        </View>
     );
 };
 const styles = StyleSheet.create({
     comment: {
         width: 395,
-        position: 'relative',
         marginTop: 5,
         justifyContent: 'center',
+        padding: 10,
+        backgroundColor: '#ffffff',
+        borderRadius: 20,
     },
 });
 
 const mapStateToProps = (state) => {
     return {
         resourceToken: state.authReducer.resourceToken,
+        userInfo: state.authReducer.userInfo,
     };
 };
 
