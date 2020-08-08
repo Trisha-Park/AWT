5a53d50c866e25fba701726c37968aeb49d005cc
Trisha Park <61039975+Trisha-Park@users.noreply.github.com>
2020-08-09 04:26:16 +0900

    :lipstick: Add Style in MyPage
    - 마이페이지에 스타일을 추가하였습니다.

--- Screens/MyPage/MyPage.js
+++ Screens/MyPage/MyPage.js
@@ -1,60 +1,67 @@
 import React from 'react';
 import { StyleSheet, Text, View, ToastAndroid, Alert } from 'react-native';
-import { Card, List, ListItem } from 'native-base';
 import { connect } from 'react-redux';
 import { signOutSuccess } from '../../Actions/authActions';
 
 const MyPage = ({ navigation, signOutSuccess, userInfo }) => {
-
     return (
         <View style={styles.container}>
-            <Card style={styles.userCard}>
-                <Card style={styles.nameCard}>
-                    <Text> {userInfo.name} 님</Text>
-                </Card>
-            </Card>
-            <View style={styles.allList}>
-                <Card>
-                    <List>
-                        <ListItem
-                            onPress={() => {
-                                navigation.navigate('MyPlans');
-                            }}
-                        >
-                            <Text>내 계획</Text>
-                        </ListItem>
-                        <ListItem
-                            onPress={() => {
-                                navigation.navigate('MyArticle');
-                            }}
-                        >
-                            <Text>내가 쓴 게시글</Text>
-                        </ListItem>
-                        <ListItem
-                            onPress={() => {
-                                navigation.navigate('MyScrap');
-                            }}
-                        >
-                            <Text>스크랩한 게시글</Text>
-                        </ListItem>
-                    </List>
-                </Card>
-                <Card>
-                    <ListItem>
-                        <Text>공지사항</Text>
-                    </ListItem>
-                    <ListItem>
-                        <Text>커뮤니티 규칙</Text>
-                    </ListItem>
-                    <ListItem>
-                        <Text>FAQ</Text>
-                    </ListItem>
-                </Card>
-                <Card>
-                    <ListItem>
-                        <Text>설정</Text>
-                    </ListItem>
-                    <ListItem
+            <View style={styles.userView}>
+                <Text
+                    style={{
+                        fontSize: 40,
+                        fontWeight: 'bold',
+                        color: '#ffffff',
+                    }}
+                >
+                    {' '}
+                    {userInfo.name} 님
+                </Text>
+            </View>
+            <View style={styles.allView}>
+                <View style={{ marginBottom: 20 }}>
+                    <View
+                        style={styles.item}
+                        onPress={() => {
+                            navigation.navigate('MyPlans');
+                        }}
+                    >
+                        <Text style={styles.fontInView}>내 계획</Text>
+                    </View>
+                    <View
+                        style={styles.item}
+                        onPress={() => {
+                            navigation.navigate('MyArticle');
+                        }}
+                    >
+                        <Text style={styles.fontInView}>내가 쓴 게시글</Text>
+                    </View>
+                    <View
+                        style={styles.item}
+                        onPress={() => {
+                            navigation.navigate('MyScrap');
+                        }}
+                    >
+                        <Text style={styles.fontInView}>스크랩한 게시글</Text>
+                    </View>
+                </View>
+                <View style={{ marginBottom: 20 }}>
+                    <View style={styles.item}>
+                        <Text style={styles.fontInView}>공지사항</Text>
+                    </View>
+                    <View style={styles.item}>
+                        <Text style={styles.fontInView}>커뮤니티 규칙</Text>
+                    </View>
+                    <View style={styles.item}>
+                        <Text style={styles.fontInView}>FAQ</Text>
+                    </View>
+                </View>
+                <View>
+                    <View style={styles.item}>
+                        <Text style={styles.fontInView}>설정</Text>
+                    </View>
+                    <View
+                        style={styles.item}
                         onPress={() => {
                             Alert.alert(
                                 '로그아웃',
@@ -85,9 +92,9 @@ const MyPage = ({ navigation, signOutSuccess, userInfo }) => {
                             );
                         }}
                     >
-                        <Text>로그아웃</Text>
-                    </ListItem>
-                </Card>
+                        <Text style={styles.fontInView}>로그아웃</Text>
+                    </View>
+                </View>
             </View>
         </View>
     );
@@ -95,33 +102,37 @@ const MyPage = ({ navigation, signOutSuccess, userInfo }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#F1F2F6',
         alignItems: 'center',
         justifyContent: 'center',
+        alignContent: 'center',
         padding: 5,
     },
-    userCard: {
-        flex: 1,
+    userView: {
+        height: 200,
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: 'lightgreen',
+        backgroundColor: '#0066FF',
         alignSelf: 'stretch',
+        borderRadius: 20,
     },
-    nameCard: {
-        height: 80,
-        width: 150,
-        alignItems: 'center',
+    allView: {
+        marginTop: 10,
+        width: 395,
+        height: 500,
+    },
+    item: {
+        marginVertical: 1,
+        backgroundColor: '#ffffff',
+        height: 50,
         justifyContent: 'center',
-        backgroundColor: 'yellowgreen',
+        paddingLeft: 10,
+        borderRadius : 5
     },
-    allList: {
-        position: 'relative',
-        flex: 3.5,
-        alignSelf: 'stretch',
-        backgroundColor: 'navy',
+    fontInView: {
+        fontSize: 26,
     },
 });
-
 
 const mapStateToProps = (state) => {
     return {
@@ -136,4 +147,3 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(MyPage);
-
